d975c3e854b8c6f142fb5b0f4ed4bb747ebb3680
kd14 <kd14@ford.com>
2024-01-31 23:39:24 +0530

    User authentication

--- account/models.py
+++ account/models.py
@@ -3,24 +3,26 @@ from django.db import models
 # Create your models here.
 from . managers import CustomUserManager
 
-from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils import timezone
 
-class CustomUser(AbstractBaseUser,PermissionsMixin):
-    
-    username=None
-    email= models.EmailField(unique=True)
-    first_name=models.CharField(max_length=80)
-    last_name=models.CharField(max_length=80)
-    is_active=models.BooleanField(default=True)
-    is_staff=models.BooleanField(default=True)
-    date_joined=models.DateTimeField(default=timezone.now)    
-    is_writer=models.BooleanField(default=False,verbose_name="Are you a Writer ?")
-    
-    USERNAME_FIELD='email'
-    REQUIRED_FIELDS=[]
-    
-    objects=CustomUserManager()
-    
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    username = None
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_writer = models.BooleanField(
+        default=False, verbose_name="Are you a Writer ?")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
     def __str__(self):
         return self.email
--- account/views.py
+++ account/views.py
@@ -1,10 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from . forms import UserCreationForm, CreateUserForm
+from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+
 
 def home(request):
-    return render(request,'account/index.html')
+    return render(request, 'account/index.html')
+
 
 def register(request):
-    return render(request,'account/register.html')
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {'RegisterForm': form}
+    return render(request, 'account/register.html', context)
+
 
 def my_login(request):
-    return render(request,'account/login.html')
+    form = AuthenticationForm()
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            # from data i getting the username and passsworf we have over rided the username to email but in forms i cant override the syntx but in email only
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None and user.is_writer == True:
+                login(request, user)
+                return HttpResponse('Welcome Writer')
+            if user is not None and user.is_writer == False:
+                login(request, user)
+                return HttpResponse('Welcome Client')
+
+    context = {'Login': form}
+    return render(request, 'account/login.html',context)
